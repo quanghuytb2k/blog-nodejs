11c032130185b74b49214f21a18fb448e7849813
quanghuy <quanghuytb2k@gmail.com>
2024-07-29 00:33:29 +0700

    handlebars template

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express')
-const app = express()
+// const hbs = require('express-handlebars')
 
-app.get('/', function (req, res) {
-  res.send('Hello World33')
+const path = require('path')
+const app = express()
+const handlebars = require('express-handlebars')
+
+
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+
+app.set('view engine','.hbs')
+app.set('views', path.join(__dirname, 'src/resources/views'));
+console.log(path.join(__dirname, 'resources/views'))
+
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+app.get('/', (req, res) => {
+  res.render('home')
 })
 
+app.get('/news', (req, res) => {
+  console.log(1122, req.query.q)
+  res.render('news')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
 app.listen(3000)
